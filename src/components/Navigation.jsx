942f528c8f16bd440208a3368d9afa790bc2f220
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-10-10 11:23:48 +0530

    First stage convert js code-->React code(html,css)

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+// Navigation.js
+import React from 'react';
+import './Navigation.css'; // Import the CSS file
+
+function Navigation() {
+  return (
+    <nav>
+      <h1>Vision Pro</h1>
+      <button>Notify Me</button>
+    </nav>
+  );
+}
+
+export default Navigation;
